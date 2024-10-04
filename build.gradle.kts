db2fcc8457ab375bffd3500296e1c4af40b6f189
sofiaotero-ascii <soterogil@danielcastelao.org>
2024-10-04 08:58:33 +0200

    PROYECTO TIERNO

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "dam.com"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Tierno_cod"
+
